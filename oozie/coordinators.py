f7497672ddccf7b9be0a7dce3c0f9415015c7212
oren mazor <oren.mazor@gmail.com>
2015-06-17 19:15:31 -0400

    add start/end attributes and some basic controls

--- oozie/coordinators.py
+++ oozie/coordinators.py
@@ -1,14 +1,27 @@
 from yattag import Doc, indent
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
 
 class Coordinator:
   def __init__(self, name, workflow, frequency):
     self.name = name
     self.workflow = workflow
     self.frequency = frequency
+    self.start = datetime.now().isoformat()
+    self.end = (datetime.now() + relativedelta(years=100)).isoformat()
 
   def as_xml(self, indentation=False):
     doc, tag, text = Doc().tagtext()
-    with tag("coordinator-app", name=self.name, frequency=str(self.frequency)):
+    with tag("coordinator-app", name=self.name, start=self.start, end=self.end, frequency=str(self.frequency)):
+      with tag("controls"):
+        with tag("timeout"):
+          text("600")
+        with tag("concurrency"):
+          text("1")
+        with tag("execution"):
+          text("FIFO")
+        with tag("throttle"):
+          text("1")
       doc.asis(self.workflow.as_xml(indent))
 
     xml = doc.getvalue()
