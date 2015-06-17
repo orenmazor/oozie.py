6c19d41536ddcf4ad9e3559695de705bfe666827
oren mazor <oren.mazor@gmail.com>
2015-06-17 14:02:36 -0400

    more things

--- oozie.py
+++ oozie.py
@@ -1,6 +1,6 @@
 from requests import post, get
 from json import loads
-from yattag import Doc
+from yattag import Doc, indent
 from sys import argv
 from sortedcontainers import SortedList
 
@@ -29,16 +29,26 @@ class Workflow:
     def add(self, action):
         self.actions.append(action)
 
-    def as_xml(self):
+    def as_xml(self, indentation=False):
         doc, tag, text = Doc().tagtext()
         with tag('workflow-app', xmlns="uri:oozie:workflow:0.4", name=self.name):
             doc.stag('start', to=self.name)
 
-            for action in self.actions:
+            for index, action in enumerate(self.actions):
                 with tag("action", name=action.name):
-                    doc.asis(action.as_xml())
+                    doc.asis(action.as_xml(indent))
+                    if index + 1 < len(self.actions):
+                      next_action = self.actions[index]
+                      doc.stag("ok", to=next_action.name)
+                    else: 
+                      doc.stag("ok", to="success")
+                    doc.stag("error", to="fail")
 
-        return doc.getvalue()
+        xml = doc.getvalue()
+        if indentation:
+          return indent(xml)
+        else:
+          return xml
 
 class ShellAction:
     def __init__(self, name, command, env, args=[]):
@@ -47,7 +57,7 @@ class ShellAction:
         self.environment_vars = env
         self.arguments = []
 
-    def as_xml(self):
+    def as_xml(self, indentation=False):
         doc, tag, text = Doc().tagtext()
         with tag('shell', xmlns="uri:oozie:shell-action:0.2"):
             with tag('exec'):
@@ -58,10 +68,13 @@ class ShellAction:
             for env in self.environment_vars:
                 with tag('env-var'):
                     text(env)
-            doc.stag('ok', to=
             doc.stag('capture-output')
 
-        return doc.getvalue()
+        xml = doc.getvalue()
+        if indentation:
+          return indent(xml)
+        else:
+          return xml
 
 
 
@@ -70,5 +83,6 @@ if __name__ == "__main__":
     serv = Oozie(argv[1])
     jerb = Workflow("fooer")
     jerb.add(ShellAction(name="build", command='ls', env=[]))
-    print jerb.as_xml()
+    jerb.add(ShellAction(name="resolve", command='whoami', env=[]))
+    print jerb.as_xml(True)
 
