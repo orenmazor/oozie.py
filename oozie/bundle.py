9bd2a933ccfacc823d9e3b5d5f4ae554c31c6505
oren mazor <oren.mazor@gmail.com>
2015-07-22 12:03:10 -0400

    :see_no_evil:

--- oozie/bundle.py
+++ oozie/bundle.py
@@ -6,17 +6,17 @@ class Bundle:
         self.name = name
         self.coordinators = ()
 
-    def add(self, coordinator):
-        self.coordinators = self.coordinators + (coordinator,)
+    def add(self, coordinator, path):
+        self.coordinators = self.coordinators + ((coordinator, path),)
 
     def as_xml(self):
         doc, tag, text = Doc().tagtext()
         doc.asis("<?xml version='1.0' encoding='UTF-8'?>")
         with tag('bundle-app', name=self.name, xmlns="uri:oozie:bundle:0.1"):
             for index, coordinator in enumerate(self.coordinators):
-                with tag("coordinator", name=coordinator.name):
+                with tag("coordinator", name=coordinator[0].name):
                     with tag("app-path"):
-                        coordinator.path()
+                        coordinator[1]
 
         return indent(doc.getvalue())
 
