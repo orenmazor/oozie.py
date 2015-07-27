5e1f576d4b8bbf2cbd2b3e997a64419527910568
oren mazor <oren.mazor@gmail.com>
2015-07-27 15:37:29 -0400

    properly set th workflowpath

--- oozie/oozie_server.py
+++ oozie/oozie_server.py
@@ -37,8 +37,8 @@ class OozieServer():
     # yeah yeah DRY... TODO
     def run(self, wf):
         hdfs = PyWebHdfsClient(host=os.environ["WEBHDFS_HOST"], port='14000', user_name='oozie')
-        deployment_path = "user/oozie/one_off_runs/{0}".format(time())
-        workflow_path = "{0}/{1}/workflow.xml".format(deployment_path, wf.name)
+        deployment_path = "user/oozie/one_off_runs/{0}/{1}".format(time(), wf.name)
+        workflow_path = "{0}/workflow.xml".format(deployment_path)
         hdfs.make_dir(deployment_path)
         hdfs.create_file(workflow_path, wf.as_xml())
         doc, tag, text = Doc().tagtext()
@@ -53,7 +53,7 @@ class OozieServer():
                 with tag("name"):
                     text("oozie.wf.application.path")
                 with tag("value"):
-                    text("/"+deployment_path)
+                    text("/"+workflow_path)
 
         configuration = doc.getvalue()
         response = post("{0}/oozie/v1/jobs".format(self.url), data=configuration, headers={'Content-Type': 'application/xml'})
