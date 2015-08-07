6b17be2a9fc4cc7aad615d7612b699b5b59406c9
oren mazor <oren.mazor@gmail.com>
2015-08-07 11:19:33 -0400

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Oozie.py',
-      version='1.1',
+      version='1.2',
       description='wrapper around submitting jobs to oozie',
       author='Oren',
       author_email='oren.mazor@shopify.com',
